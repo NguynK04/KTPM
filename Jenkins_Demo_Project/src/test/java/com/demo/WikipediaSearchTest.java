08f99099fd20150edc8c6db281d75aebdcbdc515
NguynK04 <nguyenkhoa0914044556@gmail.comcd/>
2026-03-16 02:33:04 +0700

    doi ten

--- Jenkins_Demo_Project/src/test/java/com/demo/WikipediaSearchTest.java
+++ Jenkins_Demo_Project/src/test/java/com/demo/WikipediaSearchTest.java
@@ -47,7 +47,7 @@ public class WikipediaSearchTest {
 
         String title = driver.getTitle();
         System.out.println(">>> [INFO] Tieu de trang web la: " + title);
-        assertTrue(title.contains("Jenkins"));
+        assertTrue(title.contains("Cristiano Ronaldo"));
         
         System.out.println(">>> [PASS] Test Wiki thanh cong ruc ro!");
         System.out.println(">>> [PASS] Test Wiki thanh cong, chuc mung ki chu!");
